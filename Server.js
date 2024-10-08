4f058328842f5e4a4d20dd0a9f93a3a298799fab
Pankaj Kumar <kuldeepydv15@gmail.com>
2024-10-08 11:52:07 +0530

    Port Define

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');  // Import bcryptjs
+const PORT = process.env.PORT || 5000
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -61,5 +62,5 @@ app.post('/login', async (req, res) => {
 });
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+    console.log(`Server is Running at ${PORT}`);
 });
