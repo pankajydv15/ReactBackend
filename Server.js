7b1d404e12c18f022022de5f63909db7fbdaadd2
Pankaj Kumar <kuldeepydv15@gmail.com>
2024-10-08 16:48:26 +0530

    Updated MONGO_URI and other changes

--- Server.js
+++ Server.js
@@ -20,6 +20,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
 
 // Signup route with password hashing
